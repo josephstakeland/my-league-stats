6d394e53dd8fab3c21c2d48b331e80180475eb89
josephstakeland <josephstakeland@gmail.com>
2024-02-08 18:43:30 -0500

    feat: show summoner level and iconprofile function added

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,37 +1,66 @@
 <template>
-    <div>
-      <input v-model="champ" type="text" placeholder="Ingrese el nombre del campeón" />
-      <button @click="searchSummoner">Search</button>
-      <p>Summoner Level : {{ playerData.summonerLevel }}</p>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, reactive } from 'vue';
-  import axios from 'axios';
-  
-  export default {
-    setup() {
-      const champ = ref('');
-      const playerData = reactive({ summonerLevel: null });
-      const API_KEY = 'RGAPI-106c2436-5dff-425f-bcb3-e1bebefbef64';
-  
-      function searchSummoner() {
-        const APICallString = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${champ.value}?api_key=${API_KEY}`;
-  
-        axios.get(APICallString)
-          .then(response => {
-            playerData.summonerLevel = response.data.summonerLevel;
-          })
-          .catch(error => {
-            console.log("There was an error!");
-          });
-      }
-  
-      return { champ, playerData, searchSummoner };
-    },
-  };
-  </script>
+  <div>
+    <input v-model="champ" type="text" placeholder="Ingrese el nombre del campeón" />
+    <button @click="searchSummoner">Search</button>
+
+  </div>
+  <div className="summoner_card" v-if="playerName.name || playerData.summonerLevel">
+    <p>
+      <span v-if="playerName.name">player name: {{ playerName.name }}</span>
+      <span v-else>&nbsp;</span>
+    </p>
+    <img v-if="playerIcon.profileIconId" :src="'https://ddragon.leagueoflegends.com/cdn/14.3.1/img/profileicon/' + playerIcon.profileIconId + '.png'" alt="" />
+    <p>
+      <span v-if="playerData.summonerLevel">Summoner Level: {{ playerData.summonerLevel }}</span>
+      <span v-else>&nbsp;</span>
+    </p>
+ 
+  </div>
+
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+import axios from 'axios';
+
+export default {
+  setup() {
+    const champ = ref('');
+    //add  a new property to the reactive object called "playerName" with an initial value of an empty
+    const playerData = reactive({ summonerLevel: null });
+    const playerName = reactive({name: null});
+    const playerIcon = reactive({profileIconId:null});
+
+    //api riot games
+    const API_KEY = 'RGAPI-106c2436-5dff-425f-bcb3-e1bebefbef64';
+
+
+    //function  that makes a request to the api and get data about the summoner
+    function searchSummoner() {
+      const APICallString = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${champ.value}?api_key=${API_KEY}`;
+
+
+      //using axios for  make http requests
+      axios.get(APICallString)
+        .then(response => {
+
+          //save  in the variable playerData all the data returned by the response
+          playerData.summonerLevel = response.data.summonerLevel;
+          playerName.name = response.data.name;
+          playerIcon.profileIconId = response.data.profileIconId;
+
+        })
+
+        //show ing error if the server doesn't respond or if there is any other kind of error
+        .catch(error => {
+          console.log("There was an error!");
+        });
+    }
+    // return a object  with the functions and variables that we want to use in our template
+    return { champ, playerData, playerName, playerIcon, searchSummoner };
+  },
+};
+</script>
 
 <style>
     p{
--- src/components/searchChamp.vue
+++ src/components/searchChamp.vue
@@ -1,26 +1,47 @@
 <template>
-    <div>
-      <input v-model="searchChamp" @input="updateSearchChamp" type="text" placeholder="Search...">
-      <p>{{ searchChamp }}</p>
-    </div>
-  </template>
+  <div>
+    <input v-model="champ" type="text" placeholder="Ingrese el nombre del campeón" />
+    <button @click="searchSummoner">Buscar</button>
+    <p>
+      <span v-if="playerName.name">Nombre del jugador: {{ playerName.name }}</span>
+      <span v-else>&nbsp;</span>
+    </p>
+    <p>
+      <span v-if="playerData.summonerLevel">Nivel del invocador: {{ playerData.summonerLevel }}</span>
+      <span v-else>&nbsp;</span>
+    </p>
+    <img :src="'https://ddragon.leagueoflegends.com/cdn/14.3.1/img/profileicon/' + playerData.profileIconId + '.png'" alt="" />
+  </div>
+</template>
 
-<style>
+<script>
+import { ref, reactive } from 'vue';
+import axios from 'axios';
 
+export default {
+  setup() {
+    const champ = ref('');
+    const playerData = reactive({ summonerLevel: null });
+    const playerName = reactive({ name: null });
+    const playerIcon = ref('');
 
-</style>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        searchChamp: ''
-      };
-    },
-    methods: {
-      updateSearchChamp(event) {
-        this.searchChamp = event.target.value;
-      }
+    const API_KEY = 'RGAPI-106c2436-5dff-425f-bcb3-e1bebefbef64';
+
+    function searchSummoner() {
+      const APICallString = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${champ.value}?api_key=${API_KEY}`;
+
+      axios.get(APICallString)
+        .then(response => {
+          playerData.summonerLevel = response.data.summonerLevel;
+          playerName.name = response.data.name;
+          playerIcon.profileIconId = response.data.profileIconId;
+        })
+        .catch(error => {
+          console.log("There was an error!");
+        });
     }
-  };
-  </script>
+
+    return { champ, playerData, playerName, playerIcon, searchSummoner };
+  },
+};
+</script>
