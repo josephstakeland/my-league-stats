611a0a2b97bd11bff20fb3668b98e39764854926
josephstakeland <josephstakeland@gmail.com>
2024-02-09 13:09:48 -0500

    feat: css styles added

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <input v-model="champ" type="text" placeholder="Ingrese el nombre del campeón" />
-    <button @click="searchSummoner">Search</button>
+  <div className="search_input">
+    <input v-model="summoner" type="text" placeholder="Ingrese el nombre del campeón" />
+    <label for="">LAN</label>
+    <button @click="searchSummoner">SEARCH</button>
 
   </div>
   <div className="summoner_card" v-if="playerName.name || playerData.summonerLevel">
@@ -25,19 +26,19 @@ import axios from 'axios';
 
 export default {
   setup() {
-    const champ = ref('');
+    const summoner = ref('');
     //add  a new property to the reactive object called "playerName" with an initial value of an empty
     const playerData = reactive({ summonerLevel: null });
     const playerName = reactive({name: null});
     const playerIcon = reactive({profileIconId:null});
 
     //api riot games
-    const API_KEY = 'RGAPI-106c2436-5dff-425f-bcb3-e1bebefbef64';
+    const API_KEY = 'RGAPI-68d0a77d-e7a3-4d35-8efa-d5d951505279';
 
 
     //function  that makes a request to the api and get data about the summoner
     function searchSummoner() {
-      const APICallString = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${champ.value}?api_key=${API_KEY}`;
+      const APICallString = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner.value}?api_key=${API_KEY}`;
 
 
       //using axios for  make http requests
@@ -57,7 +58,7 @@ export default {
         });
     }
     // return a object  with the functions and variables that we want to use in our template
-    return { champ, playerData, playerName, playerIcon, searchSummoner };
+    return { summoner, playerData, playerName, playerIcon, searchSummoner };
   },
 };
 </script>
@@ -68,9 +69,6 @@ export default {
     }
     input::placeholder{
         text-align: center;
-    }
-    button{
-        margin-top:1rem;
     }
 
 </style>
